c90b2175bd424eb5b3fff5e070f93638495a7d33
IbrahimShafiq <69515960+IbrahimShafiq4@users.noreply.github.com>
2024-07-15 10:14:54 -0700

    Add files via upload

---
+++ 04 - background/main.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.container');
+for(var i = 1; i <= 200; i++) {
+    const blocks = document.createElement('div');
+    blocks.classList.add('block');
+    container.appendChild(blocks);
+}
+
+function generate() {
+    anime({
+        targets: '.block',
+        translateX: function() {
+            return anime.random(-1000, 1000)
+        },
+        
+        translateY: function() {
+            return anime.random(-1000, 1000)
+        },
+        scale: function() {
+            return anime.random(1,5)
+        }
+    })
+}
